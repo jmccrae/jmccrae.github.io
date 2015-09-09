8fd615daddad76c821c4e927b8bd8cf3df926dc7
John McCrae <john@mccr.ae>
2015-09-09 14:08:22 +0100

    Update index page with new affiliation

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php include 'header.html'; ?>
             <div class="row">
                 <div class="col-lg-2 col-lg-offset-3">
+                    <a href="/me_square.jpg">
                     <img src="/me_circle.jpg" property="foaf:depiction" class="depiction" style="width:100%;"/>
+                    </a>
                 </div>
                 <div class="col-lg-4">
                     <p>
-                    I am a post-doctoral researcher at the <a href="http://www.uni-bielefeld.de/">University of Bielefeld</a> in Bielefeld, Germany.
-                    I am currently working in <a href="http://cimiano.de/doku.php" property="foaf:knows">Prof. Philipp Cimiano's</a> group, <a href="http://www.sc.cit-ec.uni-bielefeld.de/" property="foaf:workplaceHomepage">AG Semantic Computing</a>.
+                    I am a visiting research fellow at the <a href="http://www.insight-centre.org/">
+Insight Centre for Data Analytics</a> at the <a href="http://www.nuigalway.ie">
+National University of Ireland Galway</a> and a post-doctoral researcher at the <a href="http://www.uni-bielefeld.de/">University of Bielefeld</a> in Bielefeld, Germany.
+                    I am currently working with <a href="http://www.paulbuitelaar.net/">Paul Buitelaar</a>
+in the <a href="http://nlp.insight-centre.org/">Unit for Natural Language Processing</a> and
+ <a href="http://cimiano.de/doku.php" property="foaf:knows">Prof. Philipp Cimiano's</a> group, <a href="http://www.sc.cit-ec.uni-bielefeld.de/" property="foaf:workplaceHomepage">AG Semantic Computing</a>.
                     My research interests include the following:
                     </p>
                     <ul>

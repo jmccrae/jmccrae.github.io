d290fbeb5b3fa70a60b0742bfacde58a5c11d682
John P. McCrae <john@mccr.ae>
2017-01-23 16:16:17 +0000

    Add new publication

--- publications.php
+++ publications.php
@@ -1,7 +1,9 @@
 <?php include 'header.html'; ?>
 <div class="row col-lg-6 col-lg-offset-3">
 
-<div style="text-align:right;padding-bottom:10px;">Download as: <a href="publications.bib">BibTeX</a>&nbsp;&nbsp;<a href="publications.json">JSON-LD</a></div><p><b><a class="publication" href="papers/mccrae2016identifying.pdf">Identifying Poorly-Defined Concepts in WordNet with Graph Metrics</a></b>. John P. McCrae and Narumol Prangnawarat, <i>Proceedings of the First Workshop on Knowledge Extraction and Knowledge Integration (KEKI-2016)</i>, (2016).</p>
+<div style="text-align:right;padding-bottom:10px;">Download as: <a href="publications.bib">BibTeX</a>&nbsp;&nbsp;<a href="publications.json">JSON-LD</a></div><p><b><a class="publication" href="papers/arcan2016expanding.pdf">Expanding wordnets to new languages with multilingual sense disambiguation</a></b>. Mihael Arcan, John P. McCrae and Paul Buitelaar, <i>Proceedings of The 26th International Conference on Computational Linguistics</i>, (2016).</p>
+
+<p><b><a class="publication" href="papers/mccrae2016identifying.pdf">Identifying Poorly-Defined Concepts in WordNet with Graph Metrics</a></b>. John P. McCrae and Narumol Prangnawarat, <i>Proceedings of the First Workshop on Knowledge Extraction and Knowledge Integration (KEKI-2016)</i>, (2016).</p>
 
 <p><b><a class="publication" href="papers/mccrae2016lixr.pdf">LIXR: Quick, succinct conversion of XML to RDF</a></b>. John P. McCrae and Philipp Cimiano, <i>Proceedings of the ISWC 2016 Posters and Demo Track</i>, (2016).</p>
 

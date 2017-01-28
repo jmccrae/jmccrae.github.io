3f9f7d50e2da39f7c4baaacfa61ff0a4ff056e81
John McCrae <john@mccr.ae>
2017-01-28 10:46:25 +0000

    Add url for publication

--- publications.php
+++ publications.php
@@ -1,7 +1,7 @@
 <?php include 'header.html'; ?>
 <div class="row col-lg-6 col-lg-offset-3">
 
-<div style="text-align:right;padding-bottom:10px;">Download as: <a href="publications.bib">BibTeX</a>&nbsp;&nbsp;<a href="publications.json">JSON-LD</a></div><p><b><a class="publication" href="papers/arcan2016expanding.pdf">Expanding wordnets to new languages with multilingual sense disambiguation</a></b>. Mihael Arcan, John P. McCrae and Paul Buitelaar, <i>Proceedings of The 26th International Conference on Computational Linguistics</i>, (2016).</p>
+<div style="text-align:right;padding-bottom:10px;">Download as: <a href="publications.bib">BibTeX</a>&nbsp;&nbsp;<a href="publications.json">JSON-LD</a></div><p><b><a class="publication" href="https://www.aclweb.org/anthology/C/C16/C16-1010.pdf">Expanding wordnets to new languages with multilingual sense disambiguation</a></b>. Mihael Arcan, John P. McCrae and Paul Buitelaar, <i>Proceedings of The 26th International Conference on Computational Linguistics</i>, (2016).</p>
 
 <p><b><a class="publication" href="papers/mccrae2016identifying.pdf">Identifying Poorly-Defined Concepts in WordNet with Graph Metrics</a></b>. John P. McCrae and Narumol Prangnawarat, <i>Proceedings of the First Workshop on Knowledge Extraction and Knowledge Integration (KEKI-2016)</i>, (2016).</p>
 
@@ -9,107 +9,4 @@
 
 <p><b><a class="publication" href="papers/mccrae2016yuzu.pdf">Yuzu: Publishing Any Data as Linked Data</a></b>. John P. McCrae, <i>Proceedings of the ISWC 2016 Posters and Demo Track</i>, (2016).</p>
 
-<p><b><a class="publication" href="https://aclweb.org/anthology/S/S16/S16-1110.pdf">NUIG-UNLP at SemEval-2016 Task 1: Soft Alignment and Deep Learning for Semantic Textual Similarity</a></b>. John P. McCrae, Kartik Asooja, Nitish Aggarwal and Paul Buitelaar, <i>SemEval-2016</i>, (2016).</p>
-
-<p><b><a class="publication" href="http://interop2016.github.io/pdf/INTEROP-7.pdf">Linked Data and Text Mining as an Enabler for Reproducible Research</a></b>. John P. McCrae, Georgeta Bordea and Paul Buitelaar, <i>1st Workshop on Cross-Platform Text Mining and Natural Language Processing Interoperability</i>, (2016).</p>
-
-<p><b><a class="publication" href="http://www.sciencedirect.com/science/article/pii/S1570826815001420">Domain adaptation for ontology localization</a></b>. John P. McCrae, Mihael Arcan, Kartik Asooja, Jorge Gracia, Paul Buitelaar and Philipp Cimiano, <i>Web Semantics</i>, 36pp 23-31, (2016).</p>
-
-<p><b><a class="publication" href="http://typo.uni-konstanz.de/parseme/images/Meeting/2016-04-07-Struga-meeting/WG1-MCCRAE-ETAL-abstract.pdf">Representing Multiword Expressions on the Web with the OntoLex-Lemon model</a></b>. John P. McCrae, Philipp Cimiano, Paul Buitelaar and Georgeta Bordea, <i>PARSEME/ENeL workshop on MWE e-lexicons</i>, (2016).</p>
-
-<p><b><a class="publication" href="http://www.lrec-conf.org/proceedings/lrec2016/pdf/851_Paper.pdf">The Open Linguistics Working Group: Developing the Linguistic Linked Open Data Cloud</a></b>. John P. McCrae, Christian Chiarcos, Francis Bond, Philipp Cimiano, Thierry Declerck, Gerard de Melo, Jorge Gracia, Sebastian Hellmann, Bettina Klimek, Steven Moran, Petya Osenova, Antonio Pareja-Lora and Jonathan Pool, <i>10th Language Resource and Evaluation Conference (LREC)</i>, (2016).</p>
-
-<p><b><a class="publication" href="https://www.overleaf.com/read/rsnvsbdghybg">CILI: the Collaborative Interlingual Index</a></b>. Francis Bond, Piek Vossen, John P. McCrae and Christiane Fellbaum, <i>Proceedings of the Global WordNet Conference 2016</i>, (2016).</p>
-
-<p><b><a class="publication" href="https://www.overleaf.com/read/fwhzzvrrwrmw">Toward a truly multilingual Global Wordnet Grid</a></b>. Piek Vossen, Francis Bond and John P. McCrae, <i>Proceedings of the Global WordNet Conference 2016</i>, (2016).</p>
-
-<p><b><a class="publication" href="http://semantic-web-journal.net/content/multilingual-linked-data">Multilingual Linked Data (editorial)</a></b>. John P. McCrae, Steven Moran, Sebastian Hellmann and Martin Brümmer, <i>Semantic Web</i>, 6(4), pp 315-317, (2015).</p>
-
-<p><b><a class="publication" href="http://www.semantic-web-journal.net/content/lemonuby-large-interlinked-syntactically-rich-lexical-resource-ontologies-1">lemonUby - a large, interlinked, syntactically-rich lexical resource for ontologies</a></b>. Judith Eckle-Kohler, John McCrae and Christian Chiarcos, <i>Semantic Web</i>, 6(4), pp 371-378, (2015).</p>
-
-<p><b><a class="publication" href="papers/mccrae2015linghub.pdf">Linghub: a Linked Data based portal supporting the discovery of language resources</a></b>. John P. McCrae and Philipp Cimiano, <i>Proceedings of the 11th International Conference on Semantic Systems</i>, (2015).</p>
-
-<p><b><a class="publication" href="http://www.aclweb.org/anthology/W15-4207">Linking Four Heterogeneous Language Resources as Linked Data</a></b>. Benjamin Siemoneit, John P. McCrae and Philipp Cimiano, <i>Proceedings of the 4th Workshop on Linked Data in Linguistics</i>, pp 59-63, (2015).</p>
-
-<p><b><a class="publication" href="http://www.aclweb.org/anthology/W/W15/W15-4205.pdf">Reconciling Heterogeneous Descriptions of Language Resources</a></b>. John P. McCrae, Philipp Cimiano, Victor Rodriguez-Doncel, Daniel Vila-Suero, Jorge Gracia, Luca Matteis, Roberto Navigli, Andrejs Abele, Gabriela Vulcu and Paul Buitelaar, <i>Proceedings of the 4th Workshop on Linked Data in Linguistics</i>, pp 39-48, (2015).</p>
-
-<p><b><a class="publication" href="https://elex.link/elex2015/proceedings/eLex_2015_34_Cimiano+etal.pdf">Linked Terminology: Applying Linked Data Principles to Terminological Resources</a></b>. Philipp Cimiano, John P. McCrae, Victor Rodriguez-Doncel, Tatiana Gornostaya, Asuncion Gómez-Pérez, Benjamin Siemoneit and Andis Lagzdins, <i>Proceedings of eLex 2015</i>, pp 504-517, (2015).</p>
-
-<p><b><a class="publication" href="http://ceur-ws.org/Vol-1532/paper4.pdf">One ontology to bind them all: The META-SHARE OWL ontology for the interoperability of linguistic datasets on the Web</a></b>. John P. McCrae, Penny Labropoulou, Jorge Gracia, Marta Villegas, Victor Rodriguez-Doncel and Philipp Cimiano, <i>Proceedings of the 4th Workshop on the Multilingual Semantic Web</i>, (2015).</p>
-
-<p><b><a class="publication" href="http://link.springer.com/chapter/10.1007/978-3-319-18818-8_20#page-1">LIME: the Metadata Module for OntoLex</a></b>. Manuel Fiorelli, Armando Stellato, John P. McCrae, Philipp Cimiano and Maria Teresa Pazienza, <i>Proceedings of 12th Extended Semantic Web Conference</i>, (2015).</p>
-
-<p><b><a class="publication" href="http://link.springer.com/chapter/10.1007/978-3-319-17966-7_1">Language Resources and Linked Data: A Practical Perspective</a></b>. Jorge Gracia, Daniel Vila-Suero, John P. McCrae, Tiziano Flati, Ciro Baron and Milan Dojchinovski, In: <i>Knowledge Engineering and Knowledge Management</i>, (2015).</p>
-
-<p><b><a class="publication" href="http://link.springer.com/chapter/10.1007%2F978-3-662-43585-4_2#page-1">Design Patterns for Engineering the Ontology-Lexicon Interface</a></b>. John P. McCrae and Christina Unger, In: <i>Towards the Multilingual Semantic Web, eds. Paul Buitelaar, Philipp Cimiano</i>, pp 15-30, (2014).</p>
-
-<p><b><a class="publication" href="http://ceur-ws.org/Vol-1272/paper_82.pdf">Representing Swedish Lexical Resources in RDF with lemon</a></b>. Lars Borin, Dana Dannells, Markus Forsberg and John P. McCrae, <i>Proceedings of the ISWC 2014 Posters & Demonstrations Track - a track within the 13th International Semantic Web Conference</i>, pp 329-332, (2014).</p>
-
-<p><b><a class="publication" href="http://ceur-ws.org/Vol-1215/paper-05.pdf">Towards assured data quality and validation by data certification</a></b>. John P. McCrae, Cord Wiljes and Philipp Cimiano, <i>Proceedings of the 1st Workshop on Linked Data Quality</i>, (2014).</p>
-
-<p><b><a class="publication" href="http://alt.qcri.org/semeval2014/cdrom/pdf/SemEval016.pdf">Bielefeld SC: Orthonormal Topic Modelling for Grammar Induction</a></b>. John P. McCrae and Philipp Cimiano, <i>Proceedings of the 8th International Workshop on Semantic Evaluation</i>, (2014).</p>
-
-<p><b><a class="publication" href="http://www.aclweb.org/anthology/W/W14/W14-47.pdf#page=152">Default Physical Measurements in SUMO</a></b>. Francesca Quattri, Adam Pease and John P. McCrae, <i>Proceedings of 4th Workshop on Cognitive Aspects of the Lexicon</i>, (2014).</p>
-
-<p><b><a class="publication" href="http://anthology.aclweb.org/W/W14/W14-47.pdf#page=212">Modelling the Semantics of Adjectives in the Ontology-Lexicon Interface</a></b>. John P. McCrae, Christina Unger, Francesca Quattri and Philipp Cimiano, <i>Proceedings of 4th Workshop on Cognitive Aspects of the Lexicon</i>, (2014).</p>
-
-<p><b><a class="publication" href="https://github.com/jmccrae/wn-rdf-paper/raw/master/rdf-wordnet.pdf">Publishing and Linking WordNet using lemon and RDF</a></b>. John P. McCrae, Christiane Fellbaum and Philipp Cimiano, <i>Proceedings of the 3rd Workshop on Linked Data in Linguistics</i>, (2014).</p>
-
-<p><b><a class="publication" href="papers/ehrmann2014multilingual.pdf">A Multilingual Semantic Network as Linked Data: lemon-BabelNet</a></b>. Maud Ehrmann, Francesco Ceconi, Daniela Vannella, John P. McCrae, Philipp Cimiano and Roberto Navigli, <i>Proceedings of the 3rd Workshop on Linked Data in Linguistics</i>, (2014).</p>
-
-<p><b><a class="publication" href="papers/ehrmann2014representing.pdf">Representing Multilingual Data as Linked Data: the Case of BabelNet 2.0</a></b>. Maud Ehrmann, Francesco Ceconi, Daniela Vannella, John P. McCrae, Philipp Cimiano and Roberto Navigli, <i>Proceedings of the 9th Language Resource and Evaluation Conference</i>, pp 401-408, (2014).</p>
-
-<p><b><a class="publication" href="papers/vilasuero20143ld.pdf">3LD: Towards high quality, industry-ready Linguistic Linked Linguistic Data</a></b>. Daniel Vila-Suero, Victor Rodriguez-Doncel, Asunción Gómez-Pérez, Philipp Cimiano, John P. McCrae and Guadalupe Aguado-de-Cea, <i>European Data Forum 2014</i>, (2014).</p>
-
-<p><b><a class="publication" href="http://www.morganclaypool.com/doi/abs/10.2200/S00561ED1V01Y201401HLT024">Ontology-based interpretation of natural language</a></b>. Philipp Cimiano, Christina Unger and John McCrae, <i>Morgen & Claypool</i>, (2014).</p>
-
-<p><b><a class="publication" href="https://www.aclweb.org/anthology/D/D13/D13-1179.pdf">Orthonormal explicit topic analysis for cross-lingual document matching</a></b>. John McCrae, Philipp Cimiano and Roman Klinger, <i>Proceedings of the 2013 Conference on Empirical Methods in Natural Language Processing</i>, pp 1732-1742, (2013).</p>
-
-<p><b><a class="publication" href="http://ceur-ws.org/Vol-1064/Unger_lemon.pdf">A lemon lexicon for DBpedia</a></b>. Christina Unger, John McCrae, Sebastian Walter, Sara Winter and Philipp Cimiano, <i>Proceedings of 1st International Workshop on NLP and DBpedia</i>, (2013).</p>
-
-<p><b><a class="publication" href="https://lipn.univ-paris13.fr/tia2013/Proceedings/actesTIA2013.pdf">Multilingual variation in the context of linked data</a></b>. Elena Montiel-Ponsoda, John McCrae, Guadalupe Aguado-de-Cea and Jorge Gracia, <i>Proceedings of the 10th International Conference on Terminology and Artificial Intelligence</i>, pp 19-26, (2013).</p>
-
-<p><b><a class="publication" href="http://www.aclweb.org/anthology/W/W13/W13-52.pdf#page=20">Mining translations from the web of open linked data</a></b>. John P. McCrae and Philipp Cimiano, <i>Proceedings of the Joint Workshop on NLP&LOD and SWAIE: Semantic Web, Linked Open Data and Infromation Extraction</i>, pp 9-13, (2013).</p>
-
-<p><b><a class="publication" href="http://anthology.aclweb.org/W/W13/W13-5507.pdf">Releasing multimodal data as Linguistic Linked Open Data: An experience report</a></b>. Peter Menke, John P. McCrae and Philipp Cimiano, <i>Proceedings of the 2nd Workshop on Linked Data in Linguistics</i>, pp 44-52, (2013).</p>
-
-<p><b><a class="publication" href="papers/chiarcos2013towards.pdf">Towards open data for linguistics: Lexical Linked Data</a></b>. Christian Chiarcos, John McCrae, Philipp Cimiano and Christiane Fellbaum, In: <i>New Trends of Research in Ontologies and Lexical Resources</i>, pp 7-25, (2013).</p>
-
-<p><b><a class="publication" href="papers/cimiano2013role.pdf">On the role of senses in the Ontology-Lexicon</a></b>. Philipp Cimiano, John McCrae, Paul Buitelaar and Elena Montiel-Ponsoda, In: <i>New Trends of Research in Ontologies and Lexical Resources</i>, pp 43-62, (2013).</p>
-
-<p><b><a class="publication" href="http://nadir.uc3m.es/feosw2012/proceedings/FEOSWp1.pdf">Using SPIN to formalize accounting regulation on the Semantic Web</a></b>. Dennis Spohr, Philipp Cimiano, John McCrae and Sean O'Riain, <i>First International Workshop on Finance and Economics on the Semantic Web in conjunction with 9th Extended Semantic Web Conference</i>, pp 1-15, (2012).</p>
-
-<p><b><a class="publication" href="http://www.lrec-conf.org/proceedings/lrec2012/pdf/544_Paper.pdf">Collaborative semantic editing of linked data lexica</a></b>. John McCrae, Elena Montiel-Ponsoda and Philipp Cimiano, <i>Proc. of the 2012 International Conference on Language Resource and Evaluation</i>, pp 2619-2625, (2012).</p>
-
-<p><b><a class="publication" href="papers/mccrae2012three.pdf">Three steps for creating high quality ontology-lexica</a></b>. John McCrae and Philipp Cimiano, <i>Proc. of the Workshop on Collaborative Resource Development and Delivery at the 2012 International Conference on Language Resource and Evaluation</i>, (2012).</p>
-
-<p><b><a class="publication" href="papers/mccrae2013integrating.pdf">Integrating WordNet and Wiktionary with lemon</a></b>. John McCrae, Philipp Cimiano and Elena Montiel-Ponsoda, In: <i>Linked Data and Linguistics, eds. Christian Chiarcos, Sebastian Nordhoff, Sebastian Hellmann</i>, pp 25-34, (2012).</p>
-
-<p><b><a class="publication" href="papers/mccrae2012interchanging.pdf">Interchanging lexical resources on the Semantic Web</a></b>. John McCrae, Guadalupe Aguado-de-Cea, Paul Buitelaar, Philipp Cimiano, Thierry Declerck, Asunción Gómez-Pérez, Jorge Gracia, Laura Hollink, Elena Montiel-Ponsoda, Dennis Spohr and Tobias Wunner, <i>Language Resources and Evaluation</i>, 46(6), pp 701-709, (2012).</p>
-
-<p><b><a class="publication" href="papers/cimiano2011lexinfo.pdf">LexInfo: A declarative model for the lexicon-ontology interface</a></b>. Philipp Cimiano, Paul Buitelaar, John McCrae and Michael Sintek., <i>Web Semantics: Science, Services and Agents on the World Wide Web</i>, 9(1), pp 29-51, (2011).</p>
-
-<p><b><a class="publication" href="http://imap.websemanticsjournal.org/index.php/ps/article/viewFile/214/209">Challenges for the Multilingual Web of Data.</a></b>. Jorge Gracia, Elena Montiel-Ponsoda, Philipp Cimiano, Asunción Gómez-Pérez, Paul Buitelaar and John McCrae, <i>Web Semantics: Science, Services and Agents on the World Wide Web</i>, pp 63-71, (2011).</p>
-
-<p><b><a class="publication" href="papers/mccrae2011combining.pdf">Combining statistical and semantic approaches to the translation of ontologies and taxonomies</a></b>. John McCrae, Mauricio Espinoza, Elena Montiel-Ponsoda, Guadalupe Aguado-de-Cea and Philipp Cimiano, <i>Fifth Workshop on Syntax, Structure and Semantics in Statistical Translation in conjunction with 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies</i>, (2011).</p>
-
-<p><b><a class="publication" href="papers/mccrae2011linking.pdf">Linking Lexical Resources and Ontologies on the Semantic Web with lemon</a></b>. John McCrae, Dennis Spohr and Philipp Cimiano, <i>Proc. of the 8th Extended Semantic Web Conference</i>, pp 245-249, (2011).</p>
-
-<p><b><a class="publication" href="papers/buitelaar2011ontology.pdf">Ontology Lexicalization: The lemon perspective</a></b>. Paul Buitelaar, Philipp Cimiano, John McCrae, Elena Montiel-Ponsoda and Thierry Declerck, <i>Proc. of 9th International Conference on Terminology and Articial Intelligence</i>, (2011).</p>
-
-<p><b><a class="publication" href="papers/montiel2011representing.pdf">Representing Term Variation in lemon</a></b>. Elena Montiel-Ponsoda, Guadalupe Aguado-de-Cea and John McCrae, <i>Proc. of 9th International Conference on Terminology and Articial Intelligence</i>, (2011).</p>
-
-<p><b><a class="publication" href="http://ceur-ws.org/Vol-571/571-complete.pdf#page=10">CLOVA: An architecture for cross-language semantic data querying</a></b>. John McCrae, Jesus R. Campaña and Philipp Cimiano, <i>Proceedings of the 1st Workshop on the Multilingual Semantic Web</i>, pp 5-12, (2010).</p>
-
-<p><b><a class="publication" href="https://www.crcpress.com/Biosurveillance-Methods-and-Case-Studies/KassHout-Zhang/9781439800461">Navigating the Information Storm: Web-based global health surveillance in BioCaster</a></b>. Nigel Collier, Son Doan, Reiko Matsuda Goodwin, John McCrae, Mike Conway, Mika Shigematsu and Ai Kawazoe, In: <i>Biosurveillance: Methods and Case Studies, eds. Taha Kass-Hout, Xiaohui Zhang</i>, pp 291-312, (2010).</p>
-
-<p><b><a class="publication" href="papers/declerck2010ontology.pdf">Ontology-based multilingual access to financial reports for sharing business knowledge across Europe</a></b>. Thierry Declerck, Hans-Ulrich Krieger, Susan-Marie Thomas, Paul Buitelaar, Sean O'Riain, Tobias Wunner, Gilles Maguet, John McCrae, Dennis Spohr and Elena Montiel-Ponsoda, In: <i>International Financial Control Assessment applying Multilingual Ontology Frameworks</i>, pp 67-76, (2010).</p>
-
-<p><b><a class="publication" href="papers/collier2010ontology.pdf">An ontology-driven system for detecting global health events</a></b>. Nigel Collier, Reiko Matsuda Goodwin, John McCrae, Son Doan, Ai Kawazoe, Mike Conway, Asanee Kawtrakul, K. Takeuchi and D. Dien, <i>In Proc. of the 23rd International Conference on Computational Linguistics</i>, pp 215-222, (2010).</p>
-
-<p><b><a class="publication" href="papers/mccrae2009automatic.pdf">Automatic extraction of logically consistent ontologies from text corpora</a></b>. John McCrae, PhD Thesis for Graduate University of Advanced Studies (SoKenDai), (2009).</p>
-
-<p><b><a class="publication" href="http://srl-editor.googlecode.com/files/saakm.pdf">SRL Editor: A rule development tool for text mining</a></b>. John McCrae and Nigel Collier, <i>Proc. of Workshop on Semantic Authoring, Annotation and Knowledge Markup in conjunction with the 5th International Conference on Knowledge Capture</i>, (2009).</p>
-
-<p><b><a class="publication" href="http://www.biomedcentral.com/1471-2105/9/159">Synonym set extraction from the biomedical literature by lexical pattern discovery</a></b>. John McCrae and Nigel Collier, <i>BMC Bioinformatics</i>, 9(156), (2008).</p>
-
+<p><b><a class="publication" href="https://aclweb.org/anthology/S/S16/S16-1110.pdf">

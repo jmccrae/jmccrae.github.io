3b8135e18bc6da3042b54f02648abcbac50ce5f7
John P. McCrae <john@mccr.ae>
2018-02-22 18:07:11 +0000

    Start pub selector

--- pub-to-html.py
+++ pub-to-html.py
@@ -9,28 +9,28 @@ out.write("<?php include 'header.html'; ?>\n")
 out.write("<div class=\"row col-lg-6 col-lg-offset-3\">\n\n")
 out.write("<div style=\"text-align:right;\">Download as: <a href=\"publications.bib\">BibTeX</a>&nbsp;&nbsp;<a href=\"publications.json\">JSON-LD</a></div>")
 out.write("""<div style="text-align:right;padding-bottom:10px;">By Type:&nbsp;&nbsp;
-<a href="#" onclick="showall()">All</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Article')">Journal Articles</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Book')">Books</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('InCollection')">Book Chapters</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Proceedings')">Proceedings Volumes</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('InProceedings')">Conference/Workshops Articles</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('PhDThesis')">Thesis</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Misc')">Reports</a>
+<a href="#" class="arttype all" onclick="showall()">All</a>&nbsp;&nbsp;
+<a href="#" class="arttype selArticle" onclick="showonly('Article')">Journal Articles</a>&nbsp;&nbsp;
+<a href="#" class="arttype selBook" onclick="showonly('Book')">Books</a>&nbsp;&nbsp;
+<a href="#" class="arttype selInCollection" onclick="showonly('InCollection')">Book Chapters</a>&nbsp;&nbsp;
+<a href="#" class="arttype selProceedings" onclick="showonly('Proceedings')">Proceedings Volumes</a>&nbsp;&nbsp;
+<a href="#" class="arttype selInProceedings" onclick="showonly('InProceedings')">Conference/Workshops Articles</a>&nbsp;&nbsp;
+<a href="#" class="arttype selPhDThesis" onclick="showonly('PhDThesis')">Thesis</a>&nbsp;&nbsp;
+<a href="#" class="arttype selMisc" onclick="showonly('Misc')">Reports</a>
 </div>""")
 out.write("""<div style="text-align:right;padding-bottom:10px;">By Year:&nbsp;&nbsp;
-<a href="#" onclick="showall()">All</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2018')">2018</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2017')">2017</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2016')">2016</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2015')">2015</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2014')">2014</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2013')">2013</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2012')">2012</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2011')">2011</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2010')">2010</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2009')">2009</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2008')">2008</a>
+<a href="#" class="year all" onclick="showall()">All</a>&nbsp;&nbsp;
+<a href="#" class="year sely2018" onclick="showonly('y2018')">2018</a>&nbsp;&nbsp;
+<a href="#" class="year sely2017" onclick="showonly('y2017')">2017</a>&nbsp;&nbsp;
+<a href="#" class="year sely2016" onclick="showonly('y2016')">2016</a>&nbsp;&nbsp;
+<a href="#" class="year sely2015" onclick="showonly('y2015')">2015</a>&nbsp;&nbsp;
+<a href="#" class="year sely2014" onclick="showonly('y2014')">2014</a>&nbsp;&nbsp;
+<a href="#" class="year sely2013" onclick="showonly('y2013')">2013</a>&nbsp;&nbsp;
+<a href="#" class="year sely2012" onclick="showonly('y2012')">2012</a>&nbsp;&nbsp;
+<a href="#" class="year sely2011" onclick="showonly('y2011')">2011</a>&nbsp;&nbsp;
+<a href="#" class="year sely2010" onclick="showonly('y2010')">2010</a>&nbsp;&nbsp;
+<a href="#" class="year sely2009" onclick="showonly('y2009')">2009</a>&nbsp;&nbsp;
+<a href="#" class="year sely2000" onclick="showonly('y2008')">2008</a>
 </div>""")
 
 def mkclasses(paper):
@@ -129,9 +129,16 @@ out.write("""<script>
         function showonly(p) {
             $('.pub:not(.' + p + ')').slideUp();
             $('.' + p).slideDown();
+            $('.all').css('color','#00bdff');
+            $('.arttyp').css('color', '#00bdff');
+            $('.year').css('color', '#00bdff');
+            $('.sel' + p).css('color', 'black');
             }
         function showall(p) {
             $('.pub').slideDown();
+            $('.all').css('color','black');
+            $('.arttyp').css('color', '#00bdff');
+            $('.year').css('color', '#00bdff');
             }
             </script>""")
 
--- publications.php
+++ publications.php
@@ -2,27 +2,27 @@
 <div class="row col-lg-6 col-lg-offset-3">
 
 <div style="text-align:right;">Download as: <a href="publications.bib">BibTeX</a>&nbsp;&nbsp;<a href="publications.json">JSON-LD</a></div><div style="text-align:right;padding-bottom:10px;">By Type:&nbsp;&nbsp;
-<a href="#" onclick="showall()">All</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Article')">Journal Articles</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Book')">Books</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('InCollection')">Book Chapters</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Proceedings')">Proceedings Volumes</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('InProceedings')">Conference/Workshops Articles</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('PhDThesis')">Thesis</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('Misc')">Reports</a>
+<a href="#" class="arttype all" onclick="showall()">All</a>&nbsp;&nbsp;
+<a href="#" class="arttype selArticle" onclick="showonly('Article')">Journal Articles</a>&nbsp;&nbsp;
+<a href="#" class="arttype selBook" onclick="showonly('Book')">Books</a>&nbsp;&nbsp;
+<a href="#" class="arttype selInCollection" onclick="showonly('InCollection')">Book Chapters</a>&nbsp;&nbsp;
+<a href="#" class="arttype selProceedings" onclick="showonly('Proceedings')">Proceedings Volumes</a>&nbsp;&nbsp;
+<a href="#" class="arttype selInProceedings" onclick="showonly('InProceedings')">Conference/Workshops Articles</a>&nbsp;&nbsp;
+<a href="#" class="arttype selPhDThesis" onclick="showonly('PhDThesis')">Thesis</a>&nbsp;&nbsp;
+<a href="#" class="arttype selMisc" onclick="showonly('Misc')">Reports</a>
 </div><div style="text-align:right;padding-bottom:10px;">By Year:&nbsp;&nbsp;
-<a href="#" onclick="showall()">All</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2018')">2018</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2017')">2017</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2016')">2016</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2015')">2015</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2014')">2014</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2013')">2013</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2012')">2012</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2011')">2011</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2010')">2010</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2009')">2009</a>&nbsp;&nbsp;
-<a href="#" onclick="showonly('y2008')">2008</a>
+<a href="#" class="year all" onclick="showall()">All</a>&nbsp;&nbsp;
+<a href="#" class="year sely2018" onclick="showonly('y2018')">2018</a>&nbsp;&nbsp;
+<a href="#" class="year sely2017" onclick="showonly('y2017')">2017</a>&nbsp;&nbsp;
+<a href="#" class="year sely2016" onclick="showonly('y2016')">2016</a>&nbsp;&nbsp;
+<a href="#" class="year sely2015" onclick="showonly('y2015')">2015</a>&nbsp;&nbsp;
+<a href="#" class="year sely2014" onclick="showonly('y2014')">2014</a>&nbsp;&nbsp;
+<a href="#" class="year sely2013" onclick="showonly('y2013')">2013</a>&nbsp;&nbsp;
+<a href="#" class="year sely2012" onclick="showonly('y2012')">2012</a>&nbsp;&nbsp;
+<a href="#" class="year sely2011" onclick="showonly('y2011')">2011</a>&nbsp;&nbsp;
+<a href="#" class="year sely2010" onclick="showonly('y2010')">2010</a>&nbsp;&nbsp;
+<a href="#" class="year sely2009" onclick="showonly('y2009')">2009</a>&nbsp;&nbsp;
+<a href="#" class="year sely2000" onclick="showonly('y2008')">2008</a>
 </div><p class="pub y2018 InProceedings RajdeepSarkar JohnPMcCrae PaulBuitelaar"><b>A supervised approach to taxonomy extraction using word embeddings</a></b>. Rajdeep Sarkar, John P. McCrae and Paul Buitelaar, <i>Proceedings of the 11th Language Resource and Evaluation Conference (LREC)</i>, (2018 (Accepted)).</p>
 
 <p class="pub y2018 InProceedings IanWood JohnPMcCrae VladimirAndryushechkin PaulBuitelaar"><b>A Comparison Of Emotion Annotation Schemes And A New Annotated Data Set</a></b>. Ian Wood, John P. McCrae, Vladimir Andryushechkin and Paul Buitelaar, <i>Proceedings of the 11th Language Resource and Evaluation Conference (LREC)</i>, (2018 (Accepted)).</p>
@@ -173,8 +173,15 @@
         function showonly(p) {
             $('.pub:not(.' + p + ')').slideUp();
             $('.' + p).slideDown();
+            $('.all').css('color','#00bdff');
+            $('.arttyp').css('color', '#00bdff');
+            $('.year').css('color', '#00bdff');
+            $('.sel' + p).css('color', 'black');
             }
         function showall(p) {
             $('.pub').slideDown();
+            $('.all').css('color','black');
+            $('.arttyp').css('color', '#00bdff');
+            $('.year').css('color', '#00bdff');
             }
             </script><?php include 'footer.html'; ?>

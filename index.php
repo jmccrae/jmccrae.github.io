c4fcb69109784e936bdcb2259a77d3d92d947d1e
John P. McCrae <john@mccr.ae>
2018-08-22 15:30:31 +0100

    Update affiliation to DSI

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
                 </div>
                 <div class="col-lg-4">
                     <p>
-                    I am a lecturer above-the-bar at the <a href="http://www.insight-centre.org/">
+                    I am a lecturer above-the-bar at the 
+<a href="http://datascienceinstitute.ie/">Data Science Institute</a> and
+<a href="http://www.insight-centre.org/">
 Insight Centre for Data Analytics</a> at the <a href="http://www.nuigalway.ie">
 National University of Ireland Galway</a>.
                     I am currently working with <a href="http://www.paulbuitelaar.net/">Paul Buitelaar</a>
@@ -49,7 +51,9 @@ in the <a href="http://nlp.insight-centre.org/">Unit for Natural Language Proces
                     <p>You can contact me at <a href="mailto:john@mccr.ae" property="foaf:mbox">john@mccr.ae</a>.</p>
 
                     <p>ORCID: <a href="http://orcid.org/0000-0002-7227-1331">0000-0002-7227-1331</a><br>
-			Twitter: <a href="https://twitter.com/johnmccrae">@johnmccrae</a></p>
+                        Twitter: <a href="https://twitter.com/johnmccrae">@johnmccrae</a><br>
+                        <a href="https://scholar.google.com/citations?user=6naYN28AAAAJ">Google Scholar</a>
+                    </p>
                 </div>
             </div> 
 <?php include 'footer.html'; ?>

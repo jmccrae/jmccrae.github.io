7627344dbff25e658477372db60d1981eb81064e
John P. McCrae <john@mccr.ae>
2019-07-02 16:34:44 +0100

    Add projects

--- projects.php
+++ projects.php
@@ -5,22 +5,22 @@
                 <div class="col-lg-4">
                     <h1>Projects</h1>
 
-                    <div>
+                    <div class="project">
                         <a href="http://www.pret-a-llod.eu/">
                         <img src="pret-a-llod.png" alt="Prêt-à-LLOD" class="project-img"/><br/>
                         <i><b>Pr&ecirc;t-&agrave;-LLOD</b>: Making Linguistic Linked Open Data Ready-to-use</i></a>
                     </div>
-                    <div>
+                    <div class="project">
                         <a href="https://elex.is/">
                         <img src="elexis.jpeg" alt="ELEXIS" class="project-img"/><br/>
                         <i><b>ELEXIS</b>: European Lexicographic Infrastucture</i></a>
                     </div>
-                    <div>
+                    <div class="project">
                         <a href="https://en-word.net/">
                         <img src="en-wordnet.png" alt="English WordNet" class="project-img"/><br/>
                         <i><b>English WordNet</b></i></a>
                     </div>
-                    <div>
+                    <div class="project">
                         <a href="https://gitlab.insight-centre.org/saffron/saffron">
                         <img src="saffron.png" alt="Saffron" class="project-img"/><br/>
                         <i><b>Saffron</b>: Text Analysis and Insight Tool</i></a>

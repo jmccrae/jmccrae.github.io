5539006b484815c9ac5d95cb6ecca4f5eb743136
John P. McCrae <john@mccr.ae>
2019-07-02 16:22:37 +0100

    Add projects

---
+++ projects.php
@@ -0,0 +1,15 @@
+<?php include 'header.html'; ?>
+            <div class="row">
+                <div class="col-lg-2 col-lg-offset-3">
+                </div>
+                <div class="col-lg-4">
+                    <h5>Projects</h5>
+
+                    <div>
+                        <a href="http://www.pret-a-llod.eu/">
+                        <img src="pret-a-llog.png" alt="Prêt-à-LLOD"/>
+                        <i><b>Pr&ecircum;t-&agrave;-LLOD</b>: Making Linguistic Linked Open Data Ready-to-use</i></a>
+                    </div>
+                </div>
+            </div> 
+<?php include 'footer.html'; ?>

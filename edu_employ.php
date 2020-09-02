3fa1c59437d348183f62b09a4ad0a226e187e8df
John P. McCrae <john@mccr.ae>
2020-09-02 10:18:53 +0100

    Update Bio

--- edu_employ.php
+++ edu_employ.php
@@ -1,7 +1,14 @@
 <?php include 'header.html'; ?>
              <div class="row col-lg-9">
                 <div class="pageTop">
-                    <p>I am an established researcher with 8 years post-PhD experience, and deputy head of the NLP group at the Insight Centre, the largest centre for data science in Ireland. My work has focussed on the intersection of NLP and data science, and I have lead the development of the linguistic linked open data cloud, a large-scale integration of many language resources. I currently supervise five students, two of those students are co-supervised with Dr. Paul Buitelaar, the head of the NLP group who works on semantic technologies for natural language processing. One student is supervised in collaboration with Dr. Clodagh Downey at the Irish Dept. at NUI Galway, who researches Old and Middle Irish language and literature. I supervise two students as primary supervisor, supported by Dr. Mihael Arcan, a postdoctoral researcher in the NLP group. I completed my PhD within 3 years while still publishing a journal article (with 47 citations) and contributing to the BioCaster system for detecting disease outbreaks by processing texts in East Asian languages. After joining Bielefeld University in 2009, I played a leading role in at least two major scientific breakthroughs. Firstly, the development of the lemon Lexicon Model for Ontologies was a major contribution to the representation of semantics relative to natural language and is now being used by most relevant research groups and was one of the most significant outcomes of the Monnet project, an FP7 funded project. Secondly, out of the work on this topic I have been instrumental in creating the topic of linguistic linked open data as a major research theme which has been supported by over a dozen workshops and events and was a major theme of the 2016 Language Resource and Evaluation Conference (LREC). This topic lead to the Lider project, which used linguistic linked open data as an enabler for content analytics in enterprise and was funded by FP7, where I played a major role in writing the grant and in implementing the work plan. More recently, my work in linked data has played a pivotal role in obtaining funding for the ELEXIS project (under H2020-INFRAIA), where we will apply linked data technologies to lexicography. My work has lead to 73 publications, and on Google Scholar I have 1,310 total citations and an h-Index of 19, nearly all of these citations are for work that did not involve my PhD supervisor and I have co-authored with 104 co-authors from institutions around the world. This grant will help me to further consolidate my team within the Insight Centre and to continue my work on bridging the research areas of natural language processing and data science.</p>
+                    <p>I am the leader of the <a href="https://dsi.nuigalway.ie/units/uld">Unit for Linguistic Data</a> at the <a href="https://dsi.nuigalway.ie/">Data Science Institute</a> of National University of Ireland Galway. My work has focussed on the intersection of NLP and data science, and I have lead the development of the linguistic linked open data cloud, a large-scale integration of many language resources. I am the co-ordinator of the <a href="https://pret-a-llod.eu">Prêt-à-LLOD</a> project, funded by the European Union H2020 project, which aims to make linguistic linked open data ready-to-use. I am also a work package leader in the <a href="https://elex.is">ELEXIS</a> project on building a new lexicographic infrastructure for Europe. In addition, I am funded by the Irish Research Council under the Laureate program with the <a href="http://cardamom.insight-centre.org/">Cardamom</a> project focused on the development of the comparable deep models for minority and historical languages. Finally, I am a PI in the <a href="https://insight-center.org">SFI Insight Centre for Data Analytics</a> and, from 2021, a PI in the <a href="https://www.adaptcentre.ie/">SFI ADAPT Centre</a> and have active research collaborations with Fidelity Investments and Huawei.</p>
+
+<p>I completed my PhD within 3 years while still publishing a journal article (with 47 citations) and contributing to the BioCaster system for detecting disease outbreaks by processing texts in East Asian languages. After joining Bielefeld University in 2009, I played a leading role in at least two major scientific breakthroughs. Firstly, the development of the lemon Lexicon Model for Ontologies was a major contribution to the representation of semantics relative to natural language and is now being used by most relevant research groups and was one of the most significant outcomes of the Monnet project, an FP7 funded project. Secondly, out of the work on this topic I have been instrumental in creating the topic of linguistic linked open data as a major research theme which has been supported by over a dozen workshops and events and was a major theme of the 2016 Language Resource and Evaluation Conference (LREC). This topic lead to the Lider project, which used linguistic linked open data as an enabler for content analytics in enterprise and was funded by FP7, where I played a major role in writing the grant and in implementing the work plan. More recently, my work in linked data has played a pivotal role in obtaining funding for the ELEXIS project (under H2020-INFRAIA), where we will apply linked data technologies to lexicography. </p>
+
+<p>My work has lead to over 100 publications, nearly all of these citations are for work that did not involve my PhD supervisor and I have co-authored with over 150 co-authors from institutions around the world. You can see more about my publications h<a href="/publications">here</a>.</p>
+
+
+<p><a href="https://docs.google.com/document/d/1_C4ux5zLFm-ZmB-_CQM9n9fIQBxanRRDPGvSALAM-xk/edit?usp=sharing">Full CV</a></p>
 </div>
 </div>
              <div class="row col-lg-6 col-lg-offset-3">
@@ -31,32 +38,6 @@
                         <li>
                             <b>Oct 2008 to Sept 2009</b>: <a href="http://www.nii.ac.jp/">National Institute of Informatics</a> Research Assistant on the <a href="http://born.nii.ac.jp">BioCaster project</a> for infectious disease tracking
                         </li>
-                    </ul>
-                    <h2>Organization</h2>
-<ul>
-<li>Chair, 1st Conference on <a href="http://ldk2017.org/">Language, Data and Knowledge (LDK 2017)</a>-</li>
-<li>Director, 1st and 2nd Summer Datathon on Linguistic Linked Open Data (<a href="http://datathon.lider-project.eu/">SD-LLOD-15</a>,<a href="http://datathon2017.retele.linkeddata.es/">SD-LLOD-17</a>)</li>
-<li>Chair of 7 workshops (including 5 instances of the <a href="http://ldl2018.linguistic-lod.org/">Linked Data in Linguistics Series</a>) and 5 tutorials</li>
-<li>Guest lecturer at <a href="http://eurolan.info.uaic.ro/2015/">EUROLAN 2015</a> (“Ontology-Lexica with Lemon”) and <a href="http://esslli2018.folli.info/">ESSLLI 2018</a>. (“Introduction to Linked Open Data in Linguistics”)</li>
-<li>Chair of W3C Community Group on Best Practices for Multilingual Linked Open Data (BPMLOD).</li>
-<li>Member of program committee for over 60 major conferences and workshops, including ACL, EMNLP, COLING, LREC, ISWC, ESWC.</li>
-<li>Reviewer for 12 journals including Journal of Artificial Intelligence Research, Artificial Intelligence (Elsevier), Natural Language Engineering and Semantic Web Journal</li>
-<li>Guest editor of special issue of MDPI Information on “Towards the Multilingual Web of Data”</li>
-<li>Guest editor of special issue of the Semantic Web Journal on “Multilingual Linked Data”</li>
-</ul>
-                    <h2>Teaching</h2>
-<ul>
-<li>Natural Language Processing (MSc Course, NUI Galway) - Lecturer 15/16, 16/17, 17/18</li>
-<li>Statistical Natural Language Processing (MSc Course, Bielefeld University) - Lecturer 13/14, 14/15</li>
-<li>Introduction to the Semantic Web (MSc Course, Bielefeld University) - Tutor 10/11</li>
-</ul>
-
-
-
-                    <h2>Research visits</h2>
-                    <ul>
-                        <li>Princeton University, November-December 2013</li>
-                        <li>Technical University of Delft, September 2010</li>
                     </ul>
                 </div> 
             </div> 
